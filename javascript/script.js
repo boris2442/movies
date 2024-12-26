acc5a80f50e401e297b6b821cef01b9bb2650848
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-26 16:07:48 +0100

    javascript travailsur le scroll

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,29 @@
 const title=document.querySelector("h1");
-const typewriter=new Typewriter("title", {
+const typewriter=new Typewriter(title, {
     loop:true,
     delay:200
 })
 
+.typeString("Au dela des nuages")
+.pauseFor(2000)
+.deleteAll()
+.start();
+
+
+// code reservé a la librairie scrollReveal
+
+const sr=ScrollReveal({
+    duration:1500,
+    origin:"top",
+    distance:"30px",
+    reset:true
+})
+sr.reveal(".footer-text, .section3, .section2, .section1", {interval:200})
+
+
+
+// partie menu burger
+
+const menu=document.querySelector(".bars");
+const navigation=document.querySelector("header ul");
+menu
